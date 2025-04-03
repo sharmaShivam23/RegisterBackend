b3a1e812bd6af0e6f66c9c9e984fc1e4d0b04444
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-04-03 21:39:32 +0530

    o

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.use(cors({
     "http://127.0.0.1:5500",
     "http://localhost:5500",
     "http://localhost:5173",
-    "http://localhost:5174"
+    "http://localhost:5174",
+    "new-cccc.vercel.app",
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
