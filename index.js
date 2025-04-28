9a6970581a755e9116ed34d10f6e6a477286e45b
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-04-28 18:20:11 +0530

    rate

--- index.js
+++ index.js
@@ -1,4 +1,64 @@
 
+// const express = require('express');
+// const cors = require("cors");
+// const session = require('express-session');
+// const cookieParser = require('cookie-parser');
+// require('dotenv').config();
+// const fileUpload = require('express-fileupload');
+// const xss = require("xss-clean");
+// const helmet = require("helmet");
+// const hpp = require("hpp");
+// const rateLimit = require("express-rate-limit");
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cookieParser());
+
+// app.use(helmet());
+
+// app.use(xss());
+
+// app.use(hpp());
+
+// app.use(cors({
+//   origin: [
+//     "https://new-cccc.vercel.app",
+//     "http://127.0.0.1:5500",
+//     "http://localhost:5500",
+//     "http://localhost:5173",
+//     "http://localhost:5174",
+//   ],
+//   credentials: true,
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+// }));
+
+// // app.use(session({
+// //   secret: process.env.SESSION_SECRET,
+// //   resave: false,
+// //   saveUninitialized: true,
+// //   cookie: { secure: process.env.NODE_ENV === 'production', httpOnly: true },
+// // }));
+
+
+// app.use(fileUpload({
+//   useTempFiles: true,
+//   tempFileDir: './uploads',
+// }));
+
+// const routes = require("./routes/Routes");
+// app.use("/api/register", routes);
+
+// const database = require('./config/database');
+// database();
+
+
+// const PORT = process.env.PORT || 4000;
+// app.listen(PORT, () => {
+//   console.log(`Project successfully running on ${PORT}`);
+// });
+
+
 const express = require('express');
 const cors = require("cors");
 const session = require('express-session');
@@ -12,14 +72,19 @@ const rateLimit = require("express-rate-limit");
 
 const app = express();
 
+
 app.use(express.json());
 app.use(cookieParser());
 
+
 app.use(helmet());
+
 
 app.use(xss());
 
+
 app.use(hpp());
+
 
 app.use(cors({
   origin: [
@@ -33,21 +98,28 @@ app.use(cors({
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
 }));
 
-// app.use(session({
-//   secret: process.env.SESSION_SECRET,
-//   resave: false,
-//   saveUninitialized: true,
-//   cookie: { secure: process.env.NODE_ENV === 'production', httpOnly: true },
-// }));
-
-
 app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: './uploads',
 }));
 
+
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000,
+  max: 3,
+  message: {
+    status: 429,
+    message: "Too many requests from this IP, please try again after an hour."
+  },
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use(limiter);
+
+
 const routes = require("./routes/Routes");
 app.use("/api/register", routes);
+
 
 const database = require('./config/database');
 database();
@@ -57,4 +129,3 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
   console.log(`Project successfully running on ${PORT}`);
 });
-
