a019dccda3e5e4942fa685fc87a56578511a1bf2
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-05-03 22:13:04 +0530

    cor

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ app.use(cors({
     "http://localhost:5500",
     "http://localhost:5173",
     "http://localhost:5174",
+    "https://www.cccakgec.live"
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
