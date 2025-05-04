e4bd710694c989c5630941d30d3e3a455032a2e3
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-05-04 14:12:23 +0530

    max

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -8,7 +8,7 @@ const rateLimit = require('express-rate-limit');
   
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 10,
+  max: 30,
   message: {
     success: false,
     message: "Too many requests from this IP, please try again later.",
