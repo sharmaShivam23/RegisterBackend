2801d235b93e0ffe05c56e0eaf7d653d72ade48e
Student-shivamsharma <sgivamsharma06@gmail.com>
2025-05-06 15:22:50 +0530

    limit

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -16,7 +16,7 @@ const rateLimit = require('express-rate-limit');
 // });
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
-  max: 60,
+  max: 62,
   message: {
     success: false,
     message: "Too many registration attempts. Please try again after an hour."
